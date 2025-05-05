0812b8be059e2e8eaeb3af45d3b515606d7c935b
Marouane <marouanenili123@gmail.com>
2025-05-05 13:08:43 +0200

    typage

--- src/components/PortfolioGrid.tsx
+++ src/components/PortfolioGrid.tsx
@@ -1,11 +1,10 @@
 import projects from '@/content/projects';
 import ProjectCard from './ProjectCard';
-import { Project } from '@/types';
 
 export default function PortfolioGrid() {
     return (
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {projects.map((project: Project, index: number) => (
+            {projects.map((project, index) => (
                 <ProjectCard key={index} {...project} />
             ))}
         </div>
