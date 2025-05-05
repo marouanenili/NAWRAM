f53ae14caccd9dc51503bcb8874d9847a0498df6
Marouane <marouanenili123@gmail.com>
2025-05-05 15:12:54 +0200

    contourne clicable pour les blog

--- src/components/BlogPreviewCard.tsx
+++ src/components/BlogPreviewCard.tsx
@@ -8,11 +8,12 @@ type BlogPreviewCardProps = {
 };
 export default function BlogPreviewCard({ title, slug, excerpt, date }: BlogPreviewCardProps) {
     return (
+        <Link href={`/blog/${slug}`} className="block bg-slate-800 rounded-lg p-4 hover:bg-slate-700 transition">
         <div>
             <h2>{title}</h2>
             <p>{excerpt}</p>
             <small>{date}</small>
-            <Link href={`/blog/${slug}`}>Lire l'article</Link>
         </div>
+        </Link>
     );
 }
